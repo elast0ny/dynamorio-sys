3fac16f8f0d94722c4cea4947956cd930b1a674e
S.J.R. van Schaik <stephan@synkhronix.com>
2022-01-15 10:57:16 -0500

    define helper functions

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,9 @@
 #![allow(improper_ctypes)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+extern "C" {
+    pub fn dr_stdout() -> file_t;
+    pub fn dr_stderr() -> file_t;
+    pub fn dr_stdin() -> file_t;
+}
