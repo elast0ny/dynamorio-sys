4b62e82b15d2c17038f69fe11d18b3533da7a28c
S.J.R. van Schaik <stephan@synkhronix.com>
2022-01-27 05:59:40 -0500

    export OPSZ_ constants

--- build.rs
+++ build.rs
@@ -216,6 +216,7 @@ fn main() {
         .allowlist_function("opnd_.*")
         .allowlist_var("INVALID_FILE")
         .allowlist_var("OP_.*")
+        .allowlist_var("OPSZ_.*")
         .allowlist_var("SPILL_SLOT_.*")
         .allowlist_var("_USES_DR_VERSION_")
         .bitfield_enum("dr_emit_flags_t")
